bce53cefd1fdfd74d9a4ddbcf66936b40605db1e
IbrahimSharara <ibrahem.gamal1998@gmail.com>
2023-07-13 09:17:31 +0300

    flatPicker is added

---
+++ flatpickr.js
@@ -0,0 +1,95 @@
+/*
+ * <input type="text" name="StopSalesDate" class="form-control flatpickr-input" id="date" placeholder="Choose date" readonly="readonly">
+ * here change id to (date or datetime or  humanfrienndlydate ...)
+ * \
+
+(function () {
+    var currentDate = new Date().toISOString().split('T')[0];
+    'use strict'
+    //To choose date
+    flatpickr("#date", {
+     
+        minDate: currentDate,
+    });
+
+    //To choose date and time
+    flatpickr("#datetime",
+
+        {
+        enableTime: true,
+        dateFormat: "Y-m-d H:i",
+    });
+
+    //For Human Friendly dates
+    flatpickr("#humanfrienndlydate", {
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+    });
+
+    //For Date Range Picker
+    flatpickr("#daterange", {
+        mode: "range",
+        dateFormat: "Y-m-d",
+    });
+
+    //For Time Picker
+    flatpickr("#timepikcr", {
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+    });
+
+    //For Time Picker With 24hr Format
+    flatpickr("#timepickr1", {
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+        time_24hr: true
+    });
+
+    //For Time Picker With Limits
+    flatpickr("#limittime", {
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+        minTime: "16:00",
+        maxTime: "22:30",
+    });
+
+    //For DateTimePicker with Limited Time Range
+    flatpickr("#limitdatetime", {
+        enableTime: true,
+        minTime: "16:00",
+        maxTime: "22:00"
+    });
+    
+    //For Inline Calendar
+    flatpickr("#inlinecalendar", {
+        inline: true
+    });
+
+    //For Date Pickr With Week Numbers
+    flatpickr("#weeknum", {
+        weekNumbers: true,
+    });
+
+    //For Inline Time
+    flatpickr("#inlinetime", {
+        inline: true,
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+    });
+
+    //For Preloading Time
+    flatpickr("#pretime", {
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+        defaultDate: "13:45"
+    });
+
+
+})();
+
